609b540f322bee6d49929f8d181846bc363e6fa8
macastellanossalamanca <macastellanossalamanca@gmail.com>
2025-02-03 20:12:33 -0500

    Base Project
    Here you can find the skeleton of the project

---
+++ Cat App/Cat App/Cat_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cat_AppApp.swift
+//  Cat App
+//
+//  Created by Sandra Salamanca on 3/02/25.
+//
+
+import SwiftUI
+
+@main
+struct Cat_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Cat App/Cat AppUITests/Cat_AppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Cat_AppUITestsLaunchTests.swift
+//  Cat AppUITests
+//
+//  Created by Sandra Salamanca on 3/02/25.
+//
+
+import XCTest
+
+final class Cat_AppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
